fab01265e23951d58d991486e5dc4e07f8db0560
Heroygptr <69488733+Heroygptr@users.noreply.github.com>
2020-08-14 22:19:34 +0800

    删除对csv的引用
    删除csv，转而使用何振远获取的数据。

--- 涨跌势（直方图），核密度.py
+++ 涨跌势（直方图），核密度.py
@@ -1,7 +1,26 @@
+def sort_data(df,number):
+    dfn = df.reset_index().iloc[-number:,:6]  #取过去多少天数据
+    dfn = dfn.dropna()
+    dfn = dfn.reset_index().drop(columns='index')
+    dfn = dfn.sort_values(by='date', ascending=True)
+    return dfn
+''' 数据排序，df是数据，number从以前到今天的数据行数 '''
+
+def get_stock_data(stock_number):
+    df = ak.stock_zh_a_daily(symbol= stock_number , adjust="hfq")
+    return df
+'''获取股票数据，记得加引号''' 
+
+def get_index_data(index_number):
+    df = ak.stock_zh_index_daily(symbol= index_number)
+    return df
+'''获取基金数据，记得加引号 '''
+
 import pandas as pd
-df = pd.read_csv('sh601727.csv')
 df.head()
 # 剔除缺失数据
+from matplotlib import pyplot as plt
+import seaborn as sns
 df = df.dropna()
 df.head()
 df = df.reset_index().drop(columns='index')
