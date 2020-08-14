b1e247ca761e0f2b8d2851ca3cd476215a6df52b
hzy <863195037@qq.com>
2020-08-14 12:39:51 +0800

    获取数据的函数

---
+++ assignment_1何振远/get_stock_data.py
@@ -0,0 +1,9 @@
+def get_stock_data(stock_number):
+    df = ak.stock_zh_a_daily(symbol= stock_number , adjust="hfq")
+    df3 = df.reset_index().iloc[-30:,:6]  #取过去30天数据
+    df3 = df3.dropna(how='any').reset_index(drop=True) #去除空值且从零开始编号索引
+    df3 = df3.sort_values(by='date', ascending=True)
+    return df3.head()
+'''
+获取股票最近三十天的数据并显示前五行
+''' 
