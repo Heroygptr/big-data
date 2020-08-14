4668d260600344a3686e6632bb9d90bda626b4d1
hzy <863195037@qq.com>
2020-08-14 13:29:30 +0800

    获取股票最近30天的信息

--- get_stock_data.py
+++ get_stock_data.py
@@ -3,7 +3,6 @@ def get_stock_data(stock_number):
     df3 = df.reset_index().iloc[-30:,:6]  #取过去30天数据
     df3 = df3.dropna(how='any').reset_index(drop=True) #去除空值且从零开始编号索引
     df3 = df3.sort_values(by='date', ascending=True)
-    print df3.head()
     return df3
 '''
 获取股票最近三十天的数据并显示前五行
